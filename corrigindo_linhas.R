39beafdfdcf591c567d94689463c26ec44a607f0
voigtjessica <voigt.jessica@gmail.com>
2018-02-26 18:01:26 -0300

    Corrigindo falhas de exportação da base da CGU

---
+++ corrigindo_linhas.R
@@ -0,0 +1,23 @@
+#Corrigindo linhas
+
+library(data.table)
+library(dplyr)
+
+setwd("C:\\Users\\jvoig\\Documents\\Achados_e_pedidos\\import_erro")
+
+pendencias <- fread("Pendencias_CGU_importador.csv", encoding="UTF-8")
+
+
+# Tutorial com gsub e loops : http://www.endmemo.com/program/R/gsub.php
+
+# Substituindo ; e " 
+
+y <- pendencias 
+
+y$Erro <- gsub("[[:punct:]]", "", y$Erro) 
+y$interac <- gsub("[[:punct:]]", "", y$interac) 
+y$conteudo <- gsub(";", ",", y$conteudo)
+
+
+write.table(y, file="pendencias_resolv.csv", 
+            sep=";", row.names=F, na="", quote = F)
